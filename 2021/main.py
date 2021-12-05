4f8ac391eee06dbe91398cd8e78997a996068383
XinYaanZyoy <XinYaanZyoy@gmail.com>
2021-12-05 08:03:56 +0530

    day 3

--- 2021/main.py
+++ 2021/main.py
@@ -41,11 +41,101 @@ def getin1(day,flag):
 
 
 # ___DAYS___
+def day3_2(day,flag):
+    print("computing day 3 challange 2")
+    data = getin(day,flag)
+    data = data.split('\n')
+    data = [data[i] for i in range(len(data)) if data[i] != ""]
+    # print(data)
+    def bita(data,j,r):
+        if r==0:
+            count=0
+            for digit in data:
+                count = count + int(digit[j])
+            # ORDER MATTERS!!
+            if count < len(data)/2:
+                count = 0
+            if count == len(data)/2:
+                count = 1
+            if count > len(data)/2:
+                count = 1
+            return count
+        if r==1:
+            count=0
+            for digit in data:
+                count = count + int(digit[j])
+            if count < len(data)/2:
+                count = 1
+            if count == len(data)/2:
+                count = 0
+            if count > len(data)/2:
+                count = 0
+            return count
+    data_copy = data
+    for j in range(len(data[0])):
+        print(data_copy)
+        data_tmp = []
+        bit = bita(data_copy,j,0)
+        for digit in data_copy:
+            if int(digit[j]) == bit:
+                data_tmp.append(digit)
+        data_copy = data_tmp
+        if len(data_copy) == 1:
+            break
+    o2_bin = data_copy[0]
+    data_copy = data
+    for j in range(len(data[0])):
+        data_tmp = []
+        bit = bita(data_copy,j,1)
+        for digit in data_copy:
+            if int(digit[j]) == bit:
+                data_tmp.append(digit)
+        data_copy = data_tmp
+        if len(data_copy) == 1:
+            break
+    co2_bin = data_copy[0]
+    print(o2_bin, co2_bin)
+    o2_dec = [int(o2_bin[i])*(2**(len(o2_bin)-1-i)) for i in range(len(o2_bin))]
+    co2_dec = [int(co2_bin[i])*(2**(len(co2_bin)-1-i)) for i in range(len(co2_bin))]
+    o2 = 0
+    for digit in o2_dec:
+        o2 = o2 + digit
+    co2 = 0
+    for digit in co2_dec:
+        co2 = co2 + digit
+    print(o2,co2, o2*co2)
+
 
 def day3_1(day,flag):
     print("computing day 3 challange 1")
     data = getin(day,flag)
-
+    data = data.split('\n')
+    data = [data[i] for i in range(len(data)) if data[i] != ""]
+    # print(data)
+    count=[0]*len(data[0])
+    # print(count)
+    for j in range(len(data[0])):
+        for i in range(len(data)):
+            count[j] = count[j] + int(data[i][j])
+        if count[j] > len(data)/2:
+            count[j] = 1
+        else:
+            count[j] = 0
+    gamma_bin = count
+    epsilon_bin = [0 if i else 1 for i in gamma_bin]
+    print(gamma_bin, epsilon_bin)
+    gamma_dec = [gamma_bin[i]*(2**(len(gamma_bin)-1-i)) for i in range(len(gamma_bin))]
+    epsilon_dec = [epsilon_bin[i]*(2**(len(epsilon_bin)-1-i)) for i in range(len(epsilon_bin))]
+    print(gamma_dec, epsilon_dec)
+    gamma = 0
+    for digit in gamma_dec:
+        gamma = gamma + digit
+    print(gamma)
+    epsilon = 0 
+    for digit in epsilon_dec:
+        epsilon = epsilon + digit
+    print(epsilon)
+    print(gamma*epsilon)
 
 def day2_2(day,flag):
     print("computing day 2 challange 2")
@@ -120,5 +210,5 @@ if __name__ == "__main__":
         print("NEED 5 ARGS!!! [file, cookie, flag(0=tst, 1=in)], day, challenge")
         exit(0)
     cookie, flag, day, challange = sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])
-    downin(day,cookie)
+    # downin(day,cookie)
     eval("day"+str(day)+"_"+str(challange)+"(day,flag)")
